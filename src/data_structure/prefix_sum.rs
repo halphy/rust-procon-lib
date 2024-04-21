77ae7ad7373e09481720b8f48968c62994afdf1d
halphy <kondo-haruo920@g.ecc.u-tokyo.ac.jp>
2024-04-21 21:35:59 +0900

    add two-dimentional ver

--- src/data_structure/prefix_sum.rs
+++ src/data_structure/prefix_sum.rs
@@ -6,6 +6,13 @@ pub mod prefix_sum {
         s: Vec<T>,
     }
 
+    pub struct PrefixSum2D<T> {
+        h: usize,
+        w: usize,
+        s: Vec<Vec<T>>,
+    }
+
+    #[allow(dead_code)]
     impl<T> PrefixSum<T>
     where
         T: Add<Output = T> + Sub<Output = T> + Copy,
@@ -27,11 +34,37 @@ pub mod prefix_sum {
             self.s[r] - self.s[l]
         }
     }
+
+    #[allow(dead_code)]
+    impl<T> PrefixSum2D<T>
+    where
+        T: Add<Output = T> + Sub<Output = T> + Copy,
+    {
+        pub fn new(h: usize, w: usize, e: T, f: impl Fn(usize, usize) -> T) -> Self {
+            let mut s = vec![vec![e; w + 1]; h + 1];
+
+            for i in 0..h {
+                for j in 0..w {
+                    s[i + 1][j + 1] = f(i, j) + s[i][j + 1] + s[i + 1][j] - s[i][j];
+                }
+            }
+
+            Self { h, w, s }
+        }
+
+        pub fn query(&self, lx: usize, rx: usize, ly: usize, ry: usize) -> T {
+            //! Returns a sum over `[lx, rx) × [ly, ry)`
+            assert!(lx <= rx && ly <= ry);
+            assert!(rx <= self.h && ry <= self.w);
+
+            self.s[rx][ry] + self.s[lx][ly] - self.s[lx][ry] - self.s[rx][ly]
+        }
+    }
 }
 
 #[cfg(test)]
 mod test {
-    use super::prefix_sum::PrefixSum;
+    use super::prefix_sum::{PrefixSum, PrefixSum2D};
 
     #[test]
     fn test_1d_simple() {
@@ -41,5 +74,15 @@ mod test {
         assert_eq!(psum.query(1, 3), 8);
         assert_eq!(psum.query(2, 5), 21);
         assert_eq!(psum.query(0, 5), 25);
+    }
+
+    #[test]
+    fn test_2d_simple() {
+        let a = vec![vec![1, 2, 3], vec![2, 4, 6], vec![3, 6, 9]];
+        let psum = PrefixSum2D::new(3, 3, 0, |i, j| a[i][j]);
+
+        assert_eq!(psum.query(0, 2, 0, 1), 3);
+        assert_eq!(psum.query(1, 2, 1, 3), 10);
+        assert_eq!(psum.query(0, 3, 0, 3), 36);
     }
 }
