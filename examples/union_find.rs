38231d5101cff11fd97a8e7d0b1e4dfcc8c1970c
halphy <kondo-haruo920@g.ecc.u-tokyo.ac.jp>
2024-04-25 21:46:38 +0900

    implement test for uf

---
+++ examples/union_find.rs
@@ -0,0 +1,31 @@
+// competitive-verifier: PROBLEM https://judge.yosupo.jp/problem/unionfind
+
+use proconio::input;
+use rust_procon_lib::data_structure::union_find::union_find::UnionFind;
+
+fn main() {
+    input! {
+        n: usize,
+        q: usize,
+    }
+
+    for _ in 0..q {
+        input! {
+            t: usize,
+            u: usize,
+            v: usize,
+        }
+
+        let mut uf = UnionFind::new(n);
+
+        match t {
+            0 => {
+                uf.unite(u, v);
+            }
+            1 => {
+                println!("{}", if uf.is_same(u, v) { 1 } else { 0 });
+            }
+            _ => unreachable!(),
+        }
+    }
+}
